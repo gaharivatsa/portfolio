5c9c73623953fb5b2f486b89ab3c5c84c426736b
Harivatsa <gaharivatsa@gmail.com>
2024-08-13 19:06:28 +0530

    next

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -13,13 +13,13 @@ const Navbar = () => {
 
       
       <div className="flex gap-4" >
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+        <a href="https://www.linkedin.com/in/harivatsa-anandan-6856b3234/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
-        <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500">
+        <a href="https://github.com/gaharivatsa" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500">
           <FontAwesomeIcon icon={faGithub} size="2x"  />
         </a>
-        <a href="mailto:your-email@example.com" className="hover:text-red-500">
+        <a href="mailto:gaharivatsa@gmail.com" className="hover:text-red-500">
           <FontAwesomeIcon icon={faEnvelope} size="2x"/>
         </a>
       </div>
